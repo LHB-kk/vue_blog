3e25f6c6ab87cdc25096adec71339a77f7baaccc
LHB-KK <1541621052@qq.com>
2020-10-04 22:10:40 +0800

    增加了一些用户接口

---
+++ nodProject/utils/cryPassword.js
@@ -0,0 +1,18 @@
+const crypto = require('crypto')
+
+// 密匙
+const SECRET_KEY = 'lhb123#'
+
+// 加密
+
+function md5(content) {
+    let md5 = crypto.createHash('md5')
+    return md5.update(content).digest('hex')
+}
+function genPassword(password) {
+    const str = `password=${password}&key=${SECRET_KEY}`
+    return md5(str);
+}
+module.exports = {
+    genPassword
+}
